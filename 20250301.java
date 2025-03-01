b00ae992df8e1f64eb601bd19ab38111fe7928bc
Taka41802 <p280132a@gmail.com>
2025-03-01 17:53:28 +0900

    javaの学習

--- 20250301.java
+++ 20250301.java
@@ -112,3 +112,76 @@ javaでは2つ中断方法がある
 　繰り返し処理自体を中断できる
 ・continue文
 　今回の周だけを中断して次の周にいける
+
+練習問題
+weight == 60 ;           ◯
+(age1 + age2) * 2 > 60;  ◯
+age % 2 == 1;            ◯
+name == "湊";            ✖︎ name.equals("湊");
+
+
+自分の答え
+public class Main{
+    public static void main(String [] args){
+    int isHungry = 1;
+    String food = "ハンバーガー";
+    System.out.println("こんにちは");
+      if (isHungry == 0){
+          System.out.println("お腹がいっぱいです");
+      }else {
+　　　　　　System.out.println("はらぺこです");
+　　　　　　System.out.println(food + "をいただきます");
+      }
+    }
+　　　　　　System.out.println("ごちそうさまでした");
+}
+
+模範回答
+public class Main {
+    public static void main(String[] args) {
+        int isHungry = 1;
+        String food = "おにぎり";
+        System.out.println("こんにちは");
+        if (isHungry== 0) {
+            System.out.println("お腹がいっぱいです");
+        } else {
+            System.out.println("はらぺこです");
+        }
+        if (isHungry == 1) {
+            System.out.println(food + "をいただきます");
+        }
+        System.out.println("ごちそうさまでした");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        boolean tenki = true;
+        if (tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩にいきます");
+        } else
+            System.out.println("DVDを見ます");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("［メニュー］1：検索 2：登録 3：削除 4：変更>");
+        int selected = new java.util.Scanner(System.in).nextInt();
+        switch (selected) {
+            case 1:
+                System.out.println("検索します");
+                break;
+            case 2:
+                System.out.println("登録します");
+                break;
+            case 3:
+                System.out.println("削除します");
+                break;
+            case 4:
+                System.out.println("変更します");
+                break;
+        }
+    }
+}
