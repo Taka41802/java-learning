d6ebb7da38b83db6eac65c40378d664351bb6f36
Taka41802 <p280132a@gmail.com>
2025-03-02 15:18:44 +0900

    javaの学習

--- 20250302.java
+++ 20250302.java
@@ -31,3 +31,34 @@ public class Main {
         System.out.println("要素の数: " + num);
     }
 }
+
+５行目でscoresの2つ目の要素に30を代入している
+public class Main {
+    public static void main(String[] args) {
+        int[] scores;
+        scores = new int[5];
+        scores[1] = 30;
+        System.out.println(scores[1]);
+    }
+}
+
+配列の初期化
+変数の値を出す前には必ず値を代入して初期化する必要がある
+初期化していない変数を使おうとするとコンパイルエラーが起こる
+以下のコードはコンパイルエラーになる
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        System.out.println(x);
+    }
+}
+
+しかし、配列の要素は自動的に初期化されるのでコンパイルエラーにはならない
+public class Main {
+    public static void main(String[] args) {
+        int[] scores = new int[5];
+        System.out.println(scores[0]);
+    }
+}
+上記のような場合、型によって返される値が決まっている
+今回は０が返される
