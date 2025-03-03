75352525b4f46fe6325b274d70b0d0c57208479d
Taka41802 <p280132a@gmail.com>
2025-03-03 09:31:59 +0900

    javaの学習

---
+++ 20250303.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a += 5;
+        System.out.println(10 * -num);
+    }
+}
+順番に処理されるため
+a = a + 5; (a = 3+ 5)と同じ
+b = a;
+16が表示される
