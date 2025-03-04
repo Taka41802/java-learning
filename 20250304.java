d72e5ca5584ca24f7214df0c9a627b564890cc06
Taka41802 <p280132a@gmail.com>
2025-03-04 10:29:09 +0900

    javaの学習

--- 20250304.java
+++ 20250304.java
@@ -84,3 +84,14 @@ double [] weights = new double[5];
 boolean [] answers = new boolean[3];
 String [] nemes = new String[3];
 
+public class Main{
+    public static void main(String[] args){
+        int [] moneyList = new int[121902,8302,55100];
+        for(int i = 0; i = moneyList.length; i++) {
+            System.out.println(moneyList[i]);
+        }
+        for(int value : moneyList ){
+            System.out.println(value);
+        }
+    }
+}
