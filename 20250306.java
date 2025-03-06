4d78dd749a23a7e619bcbb9cb1ab8ee6da064011
Taka41802 <p280132a@gmail.com>
2025-03-06 10:41:23 +0900

    javaの学習

--- 20250306.java
+++ 20250306.java
@@ -44,14 +44,71 @@ public class Main {
     }
 }
 
+練習問題
+自分の回答
+import comment.Zenhan;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        doWarusa();
+        doTogame();
+        comment.Kouhan.callDeae();
+        comment.Kouhan.showMondokoro();
+    }
+}
 
+package comment.Zenhan;
+public static void doWarusa() {
+    System.out.println("きなこでござる。食えませんがの。");
+}
+public static void doTogame() {
+    System.out.println("この老いぼれの目はごまかせませんぞ。");
+}
 
+package comment.Kouhan;
+public static void callDeae() {
+    System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+}
+public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もういいでしょう。");
+    System.out.println("この紋所が目にはいらぬか！");
+    doTogame();    // もう一度、とがめる
+}
 
+模範回答
+import comment.Zenhan;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Zenhan.doWarusa();
+        Zenhan.doTogame();
+        comment.Kouhan.callDeae();
+        comment.Kouhan.showMondokoro();
+    }
+}
 
+package comment;
 
+public class Zenhan {
+    public static void doWarusa() {
+        System.out.println("きなこでござる。食えませんがの。");
+    }
 
+    public static void doTogame() {
+        System.out.println("この老いぼれの目はごまかせませんぞ。");
+    }
+}
 
+package comment;
 
+public class Kouhan {
+    public static void callDeae() {
+        System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+    }
 
+    public static void showMondokoro() throws Exception {
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目にはいらぬか！");
+        doTogame();    // もう一度、とがめる
+    }
+}
