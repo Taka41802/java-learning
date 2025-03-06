69c71bb8847a630af8766b7162ec5248662465fc
Taka41802 <p280132a@gmail.com>
2025-03-06 14:18:37 +0900

    javaの学習

--- 20250306.java
+++ 20250306.java
@@ -184,8 +184,49 @@ public class Matango {
     int level = 10;
 }
 
+宣言の先頭にfinalをつけると定数フィールドになる
+public class Matango {
+    int hp;
+    final int LEVEL = 10;
+}
 
+操作の宣言方法
+thisは自分自身のインスタンスという意味
+public class Hero {
+    String name;
+    int hp;
 
+    void sleep() {
+        this.hp = 100;
+        System.out.println(this.name + "は、眠って回復した！");
+    }
+}
+
+メソッドの追加
+フィールドを用いるときには明示的にthisをつける！
+public class Hero {
+    String name;
+    int hp;
+    public void sleep() {
+        this.hp = 100;
+        System.out.println(this.name + "は、眠って回復した！");
+    }
+    public void sit(int sec) {
+        this.hp += sec;
+        System.out.println(this.name + "は、" + sec + "秒座った！");
+        System.out.println("HPが" + sec + "ポイント回復した");
+    }
+    public void slip() {
+        this.hp -= 5;
+        System.out.println(this.name + "は、転んだ！");
+        System.out.println("5のダメージ！");
+    }
+    public void run() {
+        System.out.println(this.name + "は、逃げ出した！");
+        System.out.println("GAMEOVER");
+        System.out.println("最終HPは" + this.hp + "でした");
+    }
+}
 
 
 
