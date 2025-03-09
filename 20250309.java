e51caf520c4c02a1f03019f159c349f785131409
Taka41802 <p280132a@gmail.com>
2025-03-09 09:29:48 +0900

    javaの学習

--- 20250309.java
+++ 20250309.java
@@ -124,3 +124,32 @@ public class Computer extends TangibleAsset {
     }
     public String getMakerName() { return this.makerName; }
 }
+
+あ　Asset
+い　IntangileAsset　
+う  Patent
+
+        自分の回答
+public interface Thing {
+    public thing{
+        double getWeight;
+    }
+}
+
+模範回答
+public interface Thing {
+    double getWeight();
+    void setWeight(double weight);
+}
+
+public abstract class TangibleAsset extends Asset implements Thing {
+    String color;
+    double weight;
+    public TangibleAsset(String name, int price, String color) {
+        super(name, price);
+        this.color = color;
+    }
+    public String getColor() { return this.color; }
+    public double getWeight() { return this.weight; }
+    public void setWeight(double weight) { this.weight = weight; }
+}
