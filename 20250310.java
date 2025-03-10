0d09242ed49cb3428c25dab6fb094f3d721c131c
Taka41802 <p280132a@gmail.com>
2025-03-10 16:51:02 +0900

    javaの学習

--- 20250310.java
+++ 20250310.java
@@ -241,6 +241,128 @@ public class Hero {
     /* … */
 }
 
+メンバに関するアクセス修飾の定石
+・フィールドは全てprivate
+・メソッドは全てpublic
+そのほかは微調整していけば良い（クラスは特に理由がなければpublicにする）
+
+王様クラスで利用される名前フィールド
+public class King {
+    public void talk(Hero h) {
+        System.out.println("王様：ようこそ我が国へ、勇者" + h.name + "よ。");
+        System.out.println("王様：長旅疲れたであろう。");
+        System.out.println("王様：まずは城下町を見てくるとよい。ではまた会おう。");
+        h.bye();
+        /* … */
+    }
+}
+
+ヒーロークラスにgetNameメソッドを追加
+public class Hero {
+    private int hp;
+    private String name;
+    private Sword sword;
+
+    public String getName(){
+        return this.name;
+    }
+    public void bye() {
+        System.out.println("勇者は別れを告げた");
+    }
+    private void die() {
+        System.out.println(this.name + "は死んでしまった！");
+        System.out.println("GAME OVERです。");
+    }
+    void sleep() {
+        this.hp = 100;
+        System.out.println(this.name + "は眠って回復した！");
+    }
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        /* … */
+        System.out.println("お化けキノコ" + m.suffix + "から2ポイントの反撃を受けた");
+        this.hp -= 2;
+        if (this.hp <= 0 ) {
+            this.die();
+        }
+    }
+    /* … */
+}
+
+このメソッドを使うことにより
+勇者の名前を表示することができる（ゲッターメソッド）
+public class King {
+    public void talk(Hero h) {
+        System.out.println("王様：ようこそ我が国へ、勇者" + h.getName() + "よ。");
+        System.out.println("王様：長旅疲れたであろう。");
+        System.out.println("王様：まずは城下町を見てくるとよい。ではまた会おう。");
+        h.bye();
+        /* … */
+    }
+}
+
+ヒーロークラスにsetterメソッドを追加
+public class Hero {
+    private int hp;
+    private String name;
+    private Sword sword;
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void bye() {
+        System.out.println("勇者は別れを告げた");
+    }
+    private void die() {
+        System.out.println(this.name + "は死んでしまった！");
+        System.out.println("GAME OVERです。");
+    }
+    void sleep() {
+        this.hp = 100;
+        System.out.println(this.name + "は眠って回復した！");
+    }
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        /* … */
+        System.out.println("お化けキノコ" + m.suffix + "から2ポイントの反撃を受けた");
+        this.hp -= 2;
+        if (this.hp <= 0 ) {
+            this.die();
+        }
+    }
+    /* … */
+}
+
+カプセル化を行う前
+public class Hero {
+    String name;
+    /* … */
+}
+
+カプセル化を行った後
+public class Hero {
+    private String name;
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    /* … */
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
