1ac7ad687b845fea2a1b733fd902c77a31d80e73
Taka41802 <p280132a@gmail.com>
2025-03-10 13:04:19 +0900

    javaの学習

---
+++ 20250310.java
@@ -0,0 +1,117 @@
+キャラクターであるため攻撃はできるが、ファイアボールを打てる職業かわからないためエラーが起こる
+多態性ではざっくりと捉えるため、中にどのキャラクターが入っているかわからなくなってしまう
+
+        メソッドが呼びだせた場合にどんな処理が起こるのか
+public abstract class Monster {
+    public void run(){
+        System.out.println("モンスターは逃げ出した。");
+    }
+}
+
+public class Slime extends Monster {
+    public void run() {
+        System.out.println("スライムはサササっと逃げ出した。");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Slime s = new Slime(); Monster m = new Slime();
+        s.run(); m.run();
+    }
+}
+
+スライムはサササっと逃げ出した。
+スライムはサササっと逃げ出した。
+
+このようにインスタンスの中身によって動くメソッドが決まります
+どのような型に入っているかは関係ありません
+
+同一視して配列を利用する
+public class Main {
+    public static void main(String[] args) {
+        Hero h1 = new Hero();
+        Hero h2 = new Hero();
+        Thief t1 = new Thief();
+        Wizard w1 = new Wizard();
+        Wizard w2 = new Wizard();
+        // 冒険開始！
+        // まず宿屋に泊まる
+        h1.hp += 50;
+        h2.hp += 50;
+        t1.hp += 50;
+        w1.hp += 50;
+        w2.hp += 50;
+    }
+}
+
+多態性と配列を組み合わせて宿屋に泊まる
+public class Main {
+    public static void main(String[] args) {
+        Character[] c = new Character[5];
+        c[0] = new Hero();
+        c[1] = new Hero();
+        c[2] = new Thief();
+        c[3] = new Wizard();
+        c[4] = new Wizard();
+        // 宿屋に泊まる
+        for (Character ch : c) {
+            ch.hp += 50;
+        }
+    }
+}
+
+これにより今までは攻撃をする相手ごとに攻撃メソッドを作成していた
+public class Hero extends Character {
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("敵に10ポイントのダメージをあたえた！");
+        m.hp -= 10;
+    }
+    public void attack(Goblin g) {
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("敵に10ポイントのダメージをあたえた！");
+        g.hp -= 10;
+    }
+    // 以下スライム用など続く
+}
+
+モンスターならなんでも攻撃できる攻撃メソッドが出来上がる
+public class Hero extends Character {
+    public void attack(Monster m) {
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("敵に10ポイントのダメージをあたえた！");
+        m.hp -= 10;
+    }
+}
+
+
+多態性を利用することで同じ指示で異なる動作をさせることができる
+public class Main {
+    public static void main(String[] args) {
+        Monster[] monsters = new Monster[3];
+        monsters[0] = new Slime();
+        monsters[1] = new Goblin();
+        monsters[2] = new DeathBat();
+        for (Monster m : monsters) {
+            m.run();
+        }
+    }
+}
+スライムは、体をうねらせて逃げ出した。
+ゴブリンは、腕をふって逃げ出した。
+地獄コウモリは、羽ばたいて逃げ出した。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
