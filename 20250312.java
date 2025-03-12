8eecf87d8fecf64835595c4f58d507def08bbc3e
Taka41802 <p280132a@gmail.com>
2025-03-12 11:42:34 +0900

    javaの学習

--- 20250312.java
+++ 20250312.java
@@ -75,6 +75,82 @@ public class Hero {
     }
 }
 
+各インスタンスのフィールドは独立している
+public class Hero {
+    String name;
+    int hp;
+    int money;
+    /* … */
+}
+
+勇者で共通のお金を持つ場合
+public class Hero {
+    String name;
+    int hp;
+    static int money;
+    /* … */
+}
+
+上記のようにstaticをつけたフィールドは静的フィールドと呼ばれます
+①フィールド変数の実体がクラスに準備される
+静的フィールドへのアクセス方法
+public class Main {
+    public static void main(String[] args) {
+        Hero h1 = new Hero();
+        Hero h2 = new Hero();
+        System.out.println(h1.hp);
+        System.out.println(Hero.money);
+        /* … */
+    }
+}
+
+②全インスタンスに、箱の分身が準備される
+各インスタンスから静的フィールドを利用可能
+public class Main {
+    public static void main(String[] args) {
+        Hero h1 = new Hero();
+        Hero h2 = new Hero();
+        Hero.money = 100;
+        System.out.println(Hero.money);
+        System.out.println(h1.money);
+        h1.money = 300;
+        System.out.println(h2.money);
+    }
+}
+
+③インスタンスを1つも生み出さなくても共通の箱が利用できる
+newをしなくても静的フィールドは利用可能
+public class Main {
+    public static void main(String[] args) {
+        // 1人も勇者を生み出していない状況で…
+        Hero.money = 100;
+        System.out.println(Hero.money);
+    }
+}
+
+staticはメソッドにもつけられる
+所持金をランダムに設定する
+public class Hero {
+    String name;
+    int hp;
+    static int money;
+    /* … */
+    static void setRandomMoney() {
+        Hero.money = (int)(Math.random() * 1000);
+    }
+}
+
+newをせずとも静的メソッドは呼び出し可能
+public class Main {
+    public static void main(String[] args) {
+        Hero.setRandomMoney();
+        System.out.println(Hero.money);
+        Hero h1 = new Hero();
+        System.out.println(h1.money);
+    }
+}
+
+
 
 
 
