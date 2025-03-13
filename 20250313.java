cf494cab9137a569fae3ff8234cc5b3bb547c9e5
Taka41802 <p280132a@gmail.com>
2025-03-13 22:55:27 +0900

    javaの学習

--- 20250313.java
+++ 20250313.java
@@ -98,8 +98,55 @@ public class Main {
     }
 }
 
+HashMapクラスの利用
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> prefs = new HashMap<String, Integer>();
+        prefs.put("京都府", 255);
+        prefs.put("東京都", 1261);
+        prefs.put("熊本県", 181);
+        int tokyo = prefs.get("東京都");
+        System.out.println("東京都の人口は、" + tokyo);
+        prefs.remove("京都府");
+        prefs.put("熊本県", 182);
+        int kumamoto = prefs.get("熊本県");
+        System.out.println("熊本県の人口は、" + kumamoto);
+    }
+}
 
+マップに格納された情報を１つずつ取り出す
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> prefs = new HashMap<String, Integer>();
+        prefs.put("京都府", 255);
+        prefs.put("東京都", 1261);
+        prefs.put("熊本県", 182);
+        for (String key : prefs.keySet()) {
+            int value = prefs.get(key);
+            System.out.println(key + "の人口は、" + value);
+        }
+    }
+}
 
+コレクションのネスト
+リストに格納した勇者の名前を書き換える
+import java.util.*;
 
+class Hero {
+    public String name;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.name = "ミナト";
+        List<Hero> list = new ArrayList<Hero>();
+        list.add(h);
+        h.name = "スガワラ";
+        System.out.println(list.get(0).name);
+    }
+}
