97249b3a6455cbeb0025327572e1878efd3633b3
Taka41802 <p280132a@gmail.com>
2025-03-15 14:55:33 +0900

    javaの学習

--- 20250315.java
+++ 20250315.java
@@ -54,3 +54,79 @@ public class Main {
         fw.close();
     }
 }
+
+finallyブロックでcloseするとエラーが起こる
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileWriter fw = new FileWriter("data.txt");
+            fw.write("hello!");
+        } catch (Exception e) {
+            System.out.println("何らかの例外が発生しました");
+        } finally {
+            fw.close();
+        }
+    }
+}
+シンボルを見つけられません シンボル: 変数 fw 場所: クラス Main
+Main.java:11
+
+変数fwのスコープを広げるがエラー
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWriter fw = new FileWriter("data.txt");
+        try {
+            fw.write("hello!");
+        } catch (Exception e) {
+            System.out.println("何らかの例外が発生しました");
+        } finally {
+            fw.close();
+        }
+    }
+}
+例外java.io.IOExceptionは報告されません。スローするには、捕捉または宣言する必要があります
+例外java.io.IOExceptionは報告されません。スローするには、捕捉または宣言する必要があります
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWriter fw;
+        try {
+            fw = new FileWriter("data.txt");
+            fw.write("hello!");
+        } catch (Exception e) {
+            System.out.println("何らかの例外が発生しました");
+        } finally {
+            fw.close();
+        }
+    }
+}
+変数fwは初期化されていない可能性があります
+Main.java:12
+例外java.io.IOExceptionは報告されません。スローするには、捕捉または宣言する必要があります
+
+後片付け処理もtry-catchする必要がある
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("data.txt");
+            fw.write("hello!");
+        } catch (Exception e) {
+            System.out.println("何らかの例外が発生しました");
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException e) {
+                ;
+            }
+        }
+    }
+}
