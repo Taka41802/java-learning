d4df8aa8ae3d9d8c3e26ebd9223feaf35a2b884a
Taka41802 <p280132a@gmail.com>
2025-03-15 19:37:52 +0900

    javaの学習

--- 20250315.java
+++ 20250315.java
@@ -130,3 +130,41 @@ public class Main {
         }
     }
 }
+
+ファイルを開いたときだけ後片付けする
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("data.txt");
+            fw.write("hello!");
+        } catch (Exception e) {
+            System.out.println("何らかの例外が発生しました");
+        } finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                    ;
+                }
+            }
+        }
+    }
+}
+
+try-with-resources文の利用
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter("data.txt");) {
+            fw.write("hello!");
+        } catch (Exception e) {
+            System.out.println("何らかの例外が発生しました");
+        }
+    }
+}
+
+
