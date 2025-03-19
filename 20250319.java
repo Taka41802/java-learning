13dbf6aed9b04e62618f66c89ebb74e874a74874
Taka41802 <p280132a@gmail.com>
2025-03-19 16:16:22 +0900

    javaの学習

---
+++ 20250319.java
@@ -0,0 +1,16 @@
+復習
+値より大きい型の変数に代入
+public class Main {
+    public static void main(String[] args) {
+        float f = 3;
+        double d = f;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
+
+int a = 3;
+int b = a += 5;
+System.out.println(a+b);
+
+答えは16
