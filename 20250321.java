81a82d535a417dc53f446f33682908c78552f1dc
Taka41802 <p280132a@gmail.com>
2025-03-21 15:26:19 +0900

    javaの学習

--- 20250321.java
+++ 20250321.java
@@ -267,3 +267,56 @@ public class Main {
         }
     }
 }
+
+
+clone()による複製をサポートしたヒーロークラス
+    implements Cloneableはマーカーインターフェイスになる
+class Hero implements Cloneable {
+    String name;    // 名前
+    int hp;         // HP
+    Sword sword;    // 装備している武器
+
+    public Hero clone() {
+        Hero result = new Hero();
+        result.name = this.name;
+        result.hp = this.hp;
+        result.sword = this.sword;
+        return result;
+    }
+
+    public Hero() {
+        this.name = "";
+    }
+    public Hero(String name) {
+        this.name = name;
+    }
+    public Sword getSword() {
+        return this.sword;
+    }
+    public void setSword(Sword sword) {
+        this.sword = sword;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Sword {
+    private String name;
+
+    public Sword() {
+        this.name = "";
+    }
+    public Sword(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
